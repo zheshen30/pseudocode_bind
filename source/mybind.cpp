2e152216c4329047cd05c5ef5b315454692d14a4
zheshen30 <zheshen30@gmail.com>
2025-06-04 23:46:00 +0800

    Update mybind.cpp

--- source/mybind.cpp
+++ source/mybind.cpp
@@ -1,7 +1,7 @@
 #include "src_t.h"
-placeholder<1> _1;  
-placeholder<2> _2;  
-placeholder<3> _3;  
-placeholder<4> _4;  
-placeholder<5> _5;  
-placeholder<6> _6;  
+constexpr placeholder<1> _1;  
+constexpr placeholder<2> _2;  
+constexpr placeholder<3> _3;  
+constexpr placeholder<4> _4;  
+constexpr placeholder<5> _5;  
+constexpr placeholder<6> _6;  
